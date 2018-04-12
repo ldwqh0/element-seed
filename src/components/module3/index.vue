54b820705ec9fca67309b6f8f5de301276f4970f
ldwqh0 <ldwqh0@outlook.com>
2018-04-12 21:36:59 +0800

    删除不必要的东西

--- src/components/module3/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div><v-map/></div>
-</template>
-<script>
-  export default {
-
-  }
-</script>
--- src/config/http/index.js
+++ src/config/http/index.js
@@ -18,23 +18,3 @@ Vue.http.interceptors.response.use(response => {
   store.commit('loadingComplete')
   return Promise.reject(error)
 })
-
-Vue.http.interceptors.response.use(response => {
-  let err = '与服务器交互时出现错误'
-
-  if (response.data.success) {
-    if (response.data.draw) {
-      return response.data
-    } else {
-      return response.data.data
-    }
-  } else {
-    err += '错误信息' + response.error
-  }
-  store.commit('addError', err)
-  return Promise.reject(response)
-}, error => {
-  let err = '与服务器交互时出现错误，错误码：' + error.response.status
-  store.commit('addError', err)
-  return Promise.reject(error)
-})
