ad091fdd4887735477a248faeddacba5a5975195
xz <ldwqh0@outlook.com>
2018-06-01 01:34:04 +0800

    升级vue-loader至15.xx版本, 基于vue-loader15,精简了大量的配置项目

--- build/build.js
+++ build/build.js
@@ -5,7 +5,6 @@ process.env.NODE_ENV = 'production'
 
 const ora = require('ora')
 const rm = require('rimraf')
-const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
