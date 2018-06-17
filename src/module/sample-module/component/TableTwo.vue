b6047a5c0bc4be6c450f1309b5abdc19f31c0b19
xz <ldwqh0@outlook.com>
2018-06-17 09:39:29 +0800

    增加表格示例和表单示例

--- src/module/sample-module/component/TableTwo.vue
+++ src/module/sample-module/component/TableTwo.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>表格2</div>
+  <div>
+    <el-table
+      ref="multipleTable"
+      :data="tableData3"
+      tooltip-effect="dark"
+      style="width: 100%"
+      @selection-change="handleSelectionChange">
+      <el-table-column type="selection"
+                       width="55"/>
+      <el-table-column label="日期"
+                       width="120">
+        <template slot-scope="scope">{{ scope.row.date }}</template>
+      </el-table-column>
+      <el-table-column prop="name"
+                       label="姓名"
+                       width="120"/>
+      <el-table-column prop="address"
+                       label="地址"
+                       show-overflow-tooltip/>
+    </el-table>
+    <div style="margin-top: 20px">
+      <el-button @click="toggleSelection([tableData3[1], tableData3[2]])">切换第二、第三行的选中状态</el-button>
+      <el-button @click="toggleSelection()">取消选择</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,7 +32,50 @@
 
   @Component
   export default class TableTwo extends Vue {
+    tableData3 = [{
+      date: '2016-05-03',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }, {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }, {
+      date: '2016-05-04',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }, {
+      date: '2016-05-01',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }, {
+      date: '2016-05-08',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }, {
+      date: '2016-05-06',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }, {
+      date: '2016-05-07',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }]
+    multipleSelection = []
 
+    toggleSelection (rows) {
+      if (rows) {
+        rows.forEach(row => {
+          this.$refs.multipleTable.toggleRowSelection(row)
+        })
+      } else {
+        this.$refs.multipleTable.clearSelection()
+      }
+    }
+
+    handleSelectionChange (val) {
+      this.multipleSelection = val
+    }
   }
 </script>
 
