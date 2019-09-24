ea8e5a8cc8d5c86672d76d76f542c3699463f5b6
LiDong <ldwqh0@outlook.com>
2019-09-24 10:07:49 +0800

    修改版本依赖

--- src/module/sample-module/component/TableTwo.vue
+++ src/module/sample-module/component/TableTwo.vue
@@ -38,35 +38,17 @@
 
   @Component
   export default class TableTwo extends Vue {
-    tableData3 = [{
-      date: '2016-05-03',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    }, {
-      date: '2016-05-02',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    }, {
-      date: '2016-05-04',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    }, {
-      date: '2016-05-01',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    }, {
-      date: '2016-05-08',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    }, {
-      date: '2016-05-06',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    }, {
-      date: '2016-05-07',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    }]
+    get tableData3 () {
+      const a = []
+      for (let i = 0; i < 1000; i++) {
+        a.push({
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        })
+      }
+      return a
+    }
 
     multipleSelection = []
 
