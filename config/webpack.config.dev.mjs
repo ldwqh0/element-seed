9e5de84ac1938e78a30cdf7a402f80502d09801c
LiDong <ldwqh0@outlook.com>
2021-06-23 18:14:12 +0800

    修改编译的热加载和静态资源路径问题

--- config/webpack.config.dev.mjs
+++ config/webpack.config.dev.mjs
@@ -1,6 +1,9 @@
 import proxy from './proxy_dev.mjs'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 
-export default function (envParams, { mode = 'production' } = {}) {
+const __dirname = dirname(fileURLToPath(import.meta.url))
+export default function (envParams, { mode = 'development' } = {}) {
   return {
     mode,
     module: {
@@ -21,12 +24,12 @@ export default function (envParams, { mode = 'production' } = {}) {
     },
     devServer: {
       publicPath: envParams.CONTEXT_PATH,
-      contentBase: `.${envParams.CONTEXT_PATH}`,
+      contentBase: resolve(__dirname, '..', 'public'),
       overlay: {
         warnings: false,
         errors: true
       },
-      inline: false,
+      inline: true,
       // 将这个改到一个不存在的地方，才能触发后台校验
       index: 'index.html',
       port: 80,
