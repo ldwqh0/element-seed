5707815fb4ae0c87b4ed7390454fb9f799d8d608
LiDong <ldwqh0@outlook.com>
2021-07-21 17:15:26 +0800

    添加一些格式化工具的适配

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  vueIndentScriptAndStyle: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  proseWrap: 'never'
+}
