0a34612c26048880baf880d05072f6d43ac190e8
ldwqh0 <ldwqh0@outlook.com>
2022-03-22 10:29:48 +0800

    添加3.0的脚手架

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@
 <script lang="ts" setup>
   // This starter template is using Vue 3 <script setup> SFCs
   // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-  import HelloWorld from './components/HelloWorld.vue'</script>
+  import HelloWorld from './components/HelloWorld.vue'
+  // do nothing
+</script>
 
-<style>
+<style scoped lang="less">
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
